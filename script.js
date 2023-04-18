fef72ae878feeece7f79be473390a777f442fa9d
Isaiah <isozay0@gmail.com>
2023-04-18 09:29:49 -0400

    adding index.html file

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ console.log(varOne - varTwo);
 
 // EASY: Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters.
 
+const nameOne = "Bryant";
+const nameTwo = "Kobe";
+
+function compareNames(nameOne, nameTwo) {
+    if (nameOne.length > nameTwo.length) {
+        let nameDiff = nameOne.length - nameTwo.length;
+        return console.log(`${nameOne} is longer than ${nameTwo} by ${nameDiff} characters.`);
+    }else {
+        nameDiff = nameTwo.length - nameOne.length;
+        return console.log(`${nameOne} is shorter than ${nameTwo} by ${nameDiff} characters.`);
+    }
+}
+
+compareNames(nameOne,nameTwo);
+
 //MEDIUM: Write a program to tell if someone is shouting (typing in all caps), whispering (typing in all lowercase), or neither. Use prompt to get user input, and then console log whether the user was shouting, whispering, or talking normally.
+
+let userInput = prompt("type in all CAPS or all lowercase");
 
 // HARD: Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
 
